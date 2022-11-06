0de9d7cb5cd5d2d1cd92456d4a4191730070e397
boostoff <113013918+boostoff@users.noreply.github.com>
2022-11-06 19:55:56 +0300

    10_glava

---
+++ 10glava.sql
@@ -0,0 +1,11 @@
+#10.1
+SELECT с.name, sum(р.amount)
+FROM customer c LEFT OUTER JOIN payment p
+ON c.customer_id = p.customer_id
+GROUP BY c.name;
+
+#10.2
+SELECT c.name, sum(р.amount)
+FROM payment p RIGHT OUTER JOIN customer c
+ON c.customer_id = p.customer_id
+GROUP BY c.name;
